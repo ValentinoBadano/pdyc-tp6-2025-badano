eecadca1bc647180ef92f68c471abd8e501bc11a
ValentinoBadano <valentinobadano@gmail.com>
2025-07-07 22:38:06 -0300

    Godspeed! :rocket:

---
+++ gateway-service/src/test/java/ar/edu/unnoba/pdyc/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ playlist-service/src/test/java/ar/edu/unnoba/pdyc/playlist_service/PlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.playlist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
